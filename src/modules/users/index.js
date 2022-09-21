c412b758e36d7f81c9b1c46f072650b6867e257d
Luan Melo <luanmelolima@gmail.com>
2022-09-20 23:35:10 -0300

    feat: hash password to save on db

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
 import { prisma } from '~/data'
 
 export const login = async ctx => {
@@ -35,12 +37,24 @@ export const list = async (ctx) => {
 
 export const create = async (ctx) => {
   try {
+    const saltRounds = 10
+
+    const hashedPassword = await bcrypt.hash(
+      ctx.request.body.password,
+      saltRounds
+    )
+
     const user = await prisma.user.create({
-      data: ctx.request.body,
+      data: {
+        data: ctx.request.body.name,
+        email: ctx.request.body.email,
+        password: hashedPassword,
+      },
     })
 
     ctx.body = user
   } catch (err) {
+    console.log(err)
     ctx.status = 500
     ctx.body = 'Ops! Algo deu errado, tente novamente.'
   }
